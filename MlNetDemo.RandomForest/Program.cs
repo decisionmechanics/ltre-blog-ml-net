56e22bdafb77beadcb624a85b14055d28f4d49a1
Andrew Tait <andrew.tait@decisionmechanics.com>
2018-06-08 10:34:12 +0100

    Add project files.

---
+++ MlNetDemo.RandomForest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MlNetDemo.RandomForest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
